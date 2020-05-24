f0ff4d1113ce10281054478439d24a05b2a7bf66
Simay Seyrek <simayseyrek@gmail.com>
2020-05-24 18:11:29 +0300

    user class

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, id_no, name, e_mail, password, level):
+        self.name = name
+        self.level = level
+        self.e_mail = e_mail
+        self.id_no = id_no
+        self.password = password
+        book_fcn()
+
+
