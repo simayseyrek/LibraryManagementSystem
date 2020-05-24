b99c6dc8c70a5bed1b11bf7ac65f047277bfd38c
Simay Seyrek <simayseyrek@gmail.com>
2020-05-24 18:13:33 +0300

    user test

---
+++ test/User_test.py
@@ -0,0 +1,10 @@
+from User import User
+user1 = User("simay seyrek", 1, "simay@gmail.com", "20202305", "1254ss")
+print(user1.password)
+print(user1.level)
+print(user1.name)
+print(user1.e_mail)
+print(user1.id_no)
+
+
+
